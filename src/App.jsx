ff64b89b41a85df6c6d11ffbfec458613ba9af8b
Philip Olembo <pjole.kenya@gmail.com>
2026-02-05 00:21:08 +0300

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from './components/Header';
+
 export default function App() {
   return (
-    <>
+    <div className="max-w-4xl mx-auto px-4">
+      <Header />
       <h1>App coming soon..</h1>
-    </>
+    </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="text-center py-6">
+      <h1 className="text-4xl sm:text-5xl font-extrabold">Personal Project Showcase App</h1>
+    </div>
+  );
+}
